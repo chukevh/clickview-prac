d3064ba54293a12b7645db64539fdcf5d0b535be
Kevin Chu <chu.kevin.h@gmail.com>
2024-04-04 19:10:25 +1100

    Completed task 2: rendered playlist and video lists

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+'use client'
+import { PlaylistItem } from '@/components/playlist-item';
+import { useEffect, useState } from 'react';
 
 export default function PlaylistsPage() {
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    fetch("/api/playlists")
+      .then(res => res.json())
+      .then(data => setPlaylists(data))
+  }, [])
+  
+  const playlistElements = playlists.map((playlist, index) => {
+    return (
+      <PlaylistItem playlist={playlist} key={index} />
+    )
+  })
+
+  console.log(playlists)
   return (
     <>
-      <h1>Playlists route</h1>
+      <h1>Playlists</h1>
+      {playlistElements}
     </>
   );
 }
--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+import VideoItem from "@/components/video-item"
+import { useEffect, useState } from "react"
+
 export default function VideosPage() {
+  const [videos, setvideos] = useState([])
+
+  useEffect(() => {
+    fetch("/api/videos")
+      .then(res => res.json())
+      .then(data => setvideos(data))
+  }, [])
+  
+  const videoElements = videos.map((video, index) => {
+    return (
+      <VideoItem video={video} key={index} />
+    )
+  })
+
+  console.log(videos)
   return (
     <>
-      <h1>Videos route</h1>
+      <h1>Videos</h1>
+      {videoElements}
     </>
   );
 }
